5bb6a6b1888a9fd6bb1e72a0106b88e1bf9eff4d
臧艺智 <1326367714@qq.com>
2024-11-17 01:45:55 +0800

    a

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from "drizzle-kit";
 
 config({ path: '.env' });
 
-export default defineConfig({
-  schema: "./server/schema.ts",
-  out: "./server/migrations",
-  driver: "pg",
-  dbCredentials: {
-    url: process.env.POSTGRES_URL!,
-    connectionString: process.env.POSTGRES_URL!,
-  },
-});
+// export default defineConfig({
+  // schema: "./server/schema.ts",
+  // out: "./server/migrations",
+  // driver: "pg",
+  // dbCredentials: {
+    // url: process.env.POSTGRES_URL!,
+    // connectionString: process.env.POSTGRES_URL!,
+  // },
+// });
--- server/schema.ts
+++ server/schema.ts
@@ -2,6 +2,6 @@ import { pgTable } from "drizzle-orm/pg-core";
 
 
 export const posts = pgTable("posts", {
-    id: serial("id").primaryKey().notNull(),
-    title: text("title").notNull(),
+    // id: serial("id").primaryKey().notNull(),
+    // title: text("title").notNull(),
 })
